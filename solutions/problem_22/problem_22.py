a1eb9dd2b9fb313f70204e86cdfaf1d6e2086430
Sam <samvbeckmann@gmail.com>
2015-11-23 19:49:01 -0600

    Added solution to problem 22.

--- solutions/problem_22/problem_22.py
+++ solutions/problem_22/problem_22.py
@@ -1,1 +1,12 @@
-# Sample text
+import re
+
+# Solution to Project Euler problem 22
+total = 0
+with open('p022_names.txt', 'r') as file_names:
+    name_list = re.split(',', file_names.readline())
+    name_list.sort()
+    for x in range(len(name_list)):
+        temp_total = sum([ord(char) - 64 for char in name_list[x].replace('"', '')])
+        total += (x + 1) * temp_total
+
+print(total)
